582c9918e8162614a7c0cc2a9fc28b9d62f271ed
Amit Tomar <amit.tomar@iiitb.org>
2020-01-30 06:19:34 +0530

    Updates

---
+++ precache-manifest.2706964bd6ff4ea3d7675b1ebe95d615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb2c910ed39d6da085fb25db38aae774",
+    "url": "/rx-smart-decoder/index.html"
+  },
+  {
+    "revision": "fce23b0c533f46ce5c3b",
+    "url": "/rx-smart-decoder/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d7d10cc9721694883be1",
+    "url": "/rx-smart-decoder/static/js/2.0dbc2189.chunk.js"
+  },
+  {
+    "revision": "0d2cefbfe8b69355ff9258cf8fa89d51",
+    "url": "/rx-smart-decoder/static/js/2.0dbc2189.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fce23b0c533f46ce5c3b",
+    "url": "/rx-smart-decoder/static/js/main.678f82f3.chunk.js"
+  },
+  {
+    "revision": "7ae8b16c12c1bc76cfb6",
+    "url": "/rx-smart-decoder/static/js/runtime-main.c4bf72b8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rx-smart-decoder/precache-manifest.2706964bd6ff4ea3d7675b1ebe95d615.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rx-smart-decoder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
